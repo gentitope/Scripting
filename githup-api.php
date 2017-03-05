b2399674311b58edc7e0b140fa24e176264ddb1b
Temitope Oteyowo <temitope.oteyowo@Temitopes-MBP.tajawal.local>
2017-03-05 11:07:18 +0400

    updates

--- githup-api.php
+++ githup-api.php
@@ -11,17 +11,17 @@ $user = 'gentitope';
 $owner = 'gentitope';
 $repo = 'pyes2csv';
 $branch = 'Production';
-$sha = '809452715b84239fd1df336aed27a1174f7b1a63';
+$sha = 'd2a7c455a1bb8ea76ca63c2c9038be46443ff73d';
 
 // A token generated from github for API connection
 
-$token = 'd2a7c455a1bb8ea76ca63c2c9038be46443ff73d';
+$token = 'e5b9c801ae1ccf2621fdee8d39316c33ae59e5cf';
 
 // Generate the url for curl for user api
-//$curl_url = 'https://api.github.com/users/' . $user . '/repos';
+$curl_url = 'https://api.github.com/users/' . $user . '/repos';
 
 // Generate the url for curl for commits api
-$curl_url = 'https://api.github.com/repos/' . $owner . '/' . $repo . '/git/commits/' . $sha . '';
+//$curl_url = 'https://api.github.com/repos/' . $owner . '/' . $repo . '/git/commits/' . $sha . '';
 
 // Generate the url for curl for branch api
 //$curl_url = 'https://api.github.com/repos/' . $owner . '/' . $repo . '/git/refs/heads/' . $branch . '';
@@ -44,7 +44,8 @@ $output = curl_exec($ch);
 // Close the curl
 curl_close($ch);
 
-// Print the output
-print_r($output);
-var_dump($output);
+//Get data output
+$output = json_decode($output, true);
+//print_r($output);
 
+file_put_contents("$owner.txt", print_r($output, true));
