debdcdaabac51766f1f8504301c4fcdace8c842c
Temitope Oteyowo <temitope.oteyowo@Temitopes-MacBook-Pro.local>
2017-03-07 00:23:30 +0400

    assignment

---
+++ php-class/basic.php
@@ -0,0 +1,31 @@
+<?php
+
+     echo 'hello world';
+     // echo  == print i STDIN
+     // $var = [a-zAA-Z0-9]
+     $var = 12;
+     echo "NO : " . $var .PHP_EOL;
+
+     $var = "temi"
+     echo "my friend is " .$var .PHP_EOL;
+     define('TEST_CONST', 'test');   //OLD CONSTANT VAR DEFINE
+     const TRAINING_CLASS = 'PHP';   //NEW WAY CONSTANT DEFINE
+
+     echo TRAINING_CLASS;
+
+
+     $PORT = 80;
+     if ($PORT == 80) {
+
+         echo 'port is http';
+     }
+     else{
+         echo 'port is https';
+     }
+
+     //DEBUG ==> var_dump() / print_r()
+
+write a if program for a port = 80 / 443
+
+    //$var, $CONSTANT , $SWITCH CASE, $CONCAT $OPERATORS
+?>
---
+++ php-class/lesson1.php
@@ -0,0 +1,31 @@
+<?php
+
+//Value is now 8.
+
+$VAR = 8;
+
+//Add 2. Value is now 10.
+$VAR = '$VAR + 2';
+echo $VAR;
+
+//Subtract 4. Value is now 6.
+$VAR = '$VAR - 4';
+echo $VAR;
+
+//Multiply by 5. Value is now 30.
+$VAR = '$VAR * 5';
+echo $VAR;
+
+//Divide by 3. Value is now 10.
+$VAR = '$VAR / 3';
+echo $VAR;
+
+//Increment value by one. Value is now 11.
+$VAR = '$VAR + 1';
+echo $VAR;
+
+//Decrement value by one. Value is now 10.
+$VAR = '$VAR - 1';
+echo $VAR;
+
+?>
