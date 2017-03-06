17ba9e6bbafa379849624c8a91abf53ca6ae2039
Temitope Oteyowo <temitope.oteyowo@Temitopes-MacBook-Pro.local>
2017-03-07 00:40:15 +0400

    assignment

--- php-class/lesson1.php
+++ php-class/lesson1.php
@@ -5,27 +5,27 @@
 $VAR = 8;
 
 //Add 2. Value is now 10.
-$VAR = '$VAR + 2';
-echo $VAR;
+$VAR = $VAR + 2;
+echo  $VAR .PHP_EOL;
 
 //Subtract 4. Value is now 6.
-$VAR = '$VAR - 4';
-echo $VAR;
+$VAR = $VAR - 4;
+echo  $VAR .PHP_EOL;
 
 //Multiply by 5. Value is now 30.
-$VAR = '$VAR * 5';
-echo $VAR;
+$VAR = $VAR * 5;
+echo  $VAR .PHP_EOL;
 
 //Divide by 3. Value is now 10.
-$VAR = '$VAR / 3';
-echo $VAR;
+$VAR = $VAR / 3;
+echo  $VAR .PHP_EOL;
 
 //Increment value by one. Value is now 11.
-$VAR = '$VAR + 1';
-echo $VAR;
+$VAR = $VAR + 1;
+echo  $VAR .PHP_EOL;
 
 //Decrement value by one. Value is now 10.
-$VAR = '$VAR - 1';
-echo $VAR;
+$VAR = $VAR - 1;
+echo  $VAR .PHP_EOL;
 
 ?>
